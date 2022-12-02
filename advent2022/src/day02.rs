9666e9f30c2e72ad95aed7a3aa90cd69545c446c
quat1024 <quat1024@users.noreply.github.com>
2022-12-02 00:29:47 -0500

    part b its not good!

--- advent2022/src/day02.rs
+++ advent2022/src/day02.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 
 use crate::input_as_string;
 
+#[derive(Clone, Copy)]
 enum Rps {
 	Rock, Paper, Scissors
 }
@@ -52,6 +53,26 @@ impl Rps {
 			Rps::Scissors => 3,
 		}
 	}
+	
+	fn produce_win_state(&self, state: &WinState) -> Rps {
+		match self {
+			Rps::Rock => match state {
+				WinState::Tie => Rps::Rock,
+				WinState::Win => Rps::Paper,
+				WinState::Loss => Rps::Scissors
+			},
+			Rps::Paper => match state {
+				WinState::Tie => Rps::Paper,
+				WinState::Win => Rps::Scissors,
+				WinState::Loss => Rps::Rock
+			},
+			Rps::Scissors => match state {
+				WinState::Tie => Rps::Scissors,
+				WinState::Win => Rps::Rock,
+				WinState::Loss => Rps::Paper
+			}
+		}
+	}
 }
 
 impl WinState {
@@ -60,6 +81,15 @@ impl WinState {
 			WinState::Win => 6,
 			WinState::Loss => 0,
 			WinState::Tie => 3
+		}
+	}
+	
+	fn from_str(s: &str) -> WinState {
+		match s.chars().next() {
+			Some('X') => WinState::Loss,
+			Some('Y') => WinState::Tie,
+			Some('Z') => WinState::Win,
+			_ => panic!("unexpected item in bagging area")
 		}
 	}
 }
@@ -84,7 +114,26 @@ pub fn run_a() -> Result<(), Box<dyn Error>> {
 }
 
 pub fn run_b() -> Result<(), Box<dyn Error>> {
-	let input = input_as_string("01.txt");
+	let input = input_as_string("02.txt");
+	
+	//parse strategy guide for real this time
+	let strategy_guide = input.lines()
+		.map(pair_from_str_2)
+		.map(|(rps, state)| (rps, rps.produce_win_state(&state)))
+		.collect::<Vec<_>>();
+	
+	//resolve the funnies
+	let mut score = 0;
+	for entry in strategy_guide {
+		score += &entry.1.score_against(&entry.0);
+	}
+	
+	println!("score is {}", score);
 	
 	Ok(())
+}
+
+fn pair_from_str_2(s: &str) -> (Rps, WinState) {
+	let stringsplit = s.split_ascii_whitespace().collect::<Vec<_>>();
+	(Rps::from_str(stringsplit[0]), WinState::from_str(stringsplit[1]))
 }
--- advent2022/src/main.rs
+++ advent2022/src/main.rs
@@ -6,7 +6,7 @@ pub mod day01;
 pub mod day02;
 
 pub fn main() -> Result<(), Box<dyn Error>> {
-	day02::run_a()
+	day02::run_b()
 }
 
 pub fn input_as_string(input_name: &str) -> String {
