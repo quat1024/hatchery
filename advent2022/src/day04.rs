4c4af9b8ce15fa6dcc98a900edce5f76e5ec5a47
quat1024 <quat1024@users.noreply.github.com>
2022-12-04 01:44:58 -0500

    day 4 part b.unwrap().unwrap().unwrap()

--- advent2022/src/day04.rs
+++ advent2022/src/day04.rs
@@ -24,6 +24,17 @@ impl SectionAssignment {
 	fn contains(&self, other: &Self) -> bool {
 		return other.start >= self.start && other.end <= self.end;
 	}
+	
+	fn overlaps(&self, other: &Self) -> bool {
+		//It's late, and I'm tired
+		for num in self.start..=self.end {
+			if (other.start..=other.end).contains(&num) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
 }
 
 fn run_a_on(input: String) -> impl Display {
@@ -39,7 +50,15 @@ fn run_a_on(input: String) -> impl Display {
 }
 
 fn run_b_on(input: String) -> impl Display {
-	"x"
+	input
+		.lines()
+		.map(|line| {
+			//Oh god its horrible
+			let mut split = line.split(',');
+			(SectionAssignment::from_str(split.next().unwrap()).unwrap(), SectionAssignment::from_str(split.next().unwrap()).unwrap())
+		})
+		.filter(|(left, right)| left.overlaps(&right))
+		.count()
 }
 
 pub fn run_a() -> impl Display {
@@ -56,12 +75,12 @@ mod test {
 	#[test]
 	fn test() {
 		assert_eq!(run_a_on(test_input_as_string(4)).to_string(), "2");
-		//assert_eq!(run_b_on(test_input_as_string(4)).to_string(), "x");
+		assert_eq!(run_b_on(test_input_as_string(4)).to_string(), "4");
 	}
 
 	#[test]
 	fn real() {
 		assert_eq!(run_a().to_string(), "580");
-		//assert_eq!(run_b().to_string(), "x");
+		assert_eq!(run_b().to_string(), "895");
 	}
 }
