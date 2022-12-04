2637106e58082afe149985a968fe8ffb58fad5e2
quat1024 <quat1024@users.noreply.github.com>
2022-12-04 01:39:27 -0500

    day 4 a

--- advent2022/src/day03.rs
+++ advent2022/src/day03.rs
@@ -14,13 +14,10 @@ impl FromStr for Sack {
 
 	fn from_str(s: &str) -> Result<Self, Self::Err> {
 		let (left, right) = s.split_at(s.len() / 2);
-		
+
 		assert!(left.len() == right.len(), "same number of items in both sacks");
-		
-		Ok(Sack {
-			left: left.to_owned(),
-			right: right.to_owned()
-		})
+
+		Ok(Sack { left: left.to_owned(), right: right.to_owned() })
 	}
 }
 
@@ -31,7 +28,7 @@ impl Sack {
 				return item;
 			}
 		}
-		
+
 		panic!("couldn't find shared item")
 	}
 }
@@ -40,29 +37,34 @@ fn priority(item: char) -> u16 {
 	match item {
 		'a'..='z' => item as u16 - 'a' as u16 + 1,
 		'A'..='Z' => item as u16 - 'A' as u16 + 27,
-		_ => panic!("unexpected item in bagging area")
+		_ => panic!("unexpected item in bagging area"),
 	}
 }
 
 fn run_a_on(input: String) -> impl Display {
 	let sacks: Result<Vec<Sack>, _> = input.lines().map(Sack::from_str).collect();
 	let sacks = sacks.unwrap();
-	
+
 	sacks.iter().map(Sack::find_same_item).map(priority).sum::<u16>()
 }
 
 fn run_b_on(input: String) -> impl Display {
 	//divide the input into groups of three
-	
-	input.lines().array_chunks().map(|[a, b, c]| {
-		for item in a.chars() {
-			if b.contains(item) && c.contains(item) {
-				return item;
+
+	input
+		.lines()
+		.array_chunks()
+		.map(|[a, b, c]| {
+			for item in a.chars() {
+				if b.contains(item) && c.contains(item) {
+					return item;
+				}
 			}
-		}
-		
-		panic!("couldnt find shared item")
-	}).map(priority).sum::<u16>()
+
+			panic!("couldnt find shared item")
+		})
+		.map(priority)
+		.sum::<u16>()
 }
 
 pub fn run_a() -> impl Display {
@@ -81,7 +83,7 @@ mod test {
 		assert_eq!(priority('p'), 16);
 		assert_eq!(priority('L'), 38);
 	}
-	
+
 	#[test]
 	fn test() {
 		assert_eq!(run_a_on(test_input_as_string(3)).to_string(), "157");
--- advent2022/src/day04.rs
+++ advent2022/src/day04.rs
@@ -1,9 +1,41 @@
+use std::convert::Infallible;
 use std::fmt::Display;
+use std::str::FromStr;
 
 use crate::*;
 
+struct SectionAssignment {
+	start: u16,
+	end: u16,
+}
+
+impl FromStr for SectionAssignment {
+	type Err = Infallible;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		let mut split = s.split('-');
+
+		//Least unidiomatic Rust code
+		Ok(SectionAssignment { start: split.next().unwrap().parse().unwrap(), end: split.next().unwrap().parse().unwrap() })
+	}
+}
+
+impl SectionAssignment {
+	fn contains(&self, other: &Self) -> bool {
+		return other.start >= self.start && other.end <= self.end;
+	}
+}
+
 fn run_a_on(input: String) -> impl Display {
-	"x"
+	input
+		.lines()
+		.map(|line| {
+			//Oh god its horrible
+			let mut split = line.split(',');
+			(SectionAssignment::from_str(split.next().unwrap()).unwrap(), SectionAssignment::from_str(split.next().unwrap()).unwrap())
+		})
+		.filter(|(left, right)| left.contains(&right) || right.contains(&left))
+		.count()
 }
 
 fn run_b_on(input: String) -> impl Display {
@@ -11,25 +43,25 @@ fn run_b_on(input: String) -> impl Display {
 }
 
 pub fn run_a() -> impl Display {
-	run_a_on(input_as_string(3))
+	run_a_on(input_as_string(4))
 }
 
 pub fn run_b() -> impl Display {
-	run_b_on(input_as_string(3))
+	run_b_on(input_as_string(4))
 }
 
 mod test {
-	//use super::*;
-	
+	use super::*;
+
 	#[test]
 	fn test() {
-		//assert_eq!(run_a_on(test_input_as_string(3)).to_string(), "157");
-		//assert_eq!(run_b_on(test_input_as_string(3)).to_string(), "70");
+		assert_eq!(run_a_on(test_input_as_string(4)).to_string(), "2");
+		//assert_eq!(run_b_on(test_input_as_string(4)).to_string(), "x");
 	}
 
 	#[test]
 	fn real() {
-		//assert_eq!(run_a().to_string(), "7811");
-		//assert_eq!(run_b().to_string(), "2639");
+		assert_eq!(run_a().to_string(), "580");
+		//assert_eq!(run_b().to_string(), "x");
 	}
 }
