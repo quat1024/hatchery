7e0576eae6f703e589e6ccbeb3f4e006ad72da0b
quat1024 <quat1024@users.noreply.github.com>
2022-12-07 02:16:53 -0500

    06 b, clumsily

--- advent2022/src/day06.rs
+++ advent2022/src/day06.rs
@@ -8,12 +8,31 @@ fn run_a_on(input: String) -> impl Display {
 			return idx.to_string();
 		}
 	}
-	
+
 	"not found".into()
 }
 
 fn run_b_on(input: String) -> impl Display {
-	"x"
+	//goddammit no cute tricks this time LOL
+
+	//random-access string
+	let input = input.chars().collect::<Vec<_>>();
+	let window_size = 14;
+
+	let mut set: std::collections::HashSet<char> = Default::default();
+
+	for start in 0..input.len() - window_size {
+		set.clear();
+		for c_idx in start..start + window_size {
+			set.insert(input[c_idx]);
+		}
+
+		if set.len() == window_size {
+			return (start + window_size).to_string();
+		}
+	}
+
+	"not found".into()
 }
 
 pub fn run_a() -> impl Display {
@@ -31,12 +50,12 @@ mod test {
 	#[test]
 	fn test() {
 		assert_eq!(run_a_on(test_input_as_string(6)).to_string(), "7");
-		assert_eq!(run_b_on(test_input_as_string(6)).to_string(), "x");
+		assert_eq!(run_b_on(test_input_as_string(6)).to_string(), "19");
 	}
 
 	#[test]
 	fn real() {
 		assert_eq!(run_a().to_string(), "1760");
-		assert_eq!(run_b().to_string(), "x");
+		assert_eq!(run_b().to_string(), "2974");
 	}
 }
