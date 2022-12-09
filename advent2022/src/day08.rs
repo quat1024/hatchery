5095be91313b8d43df3e87ece5a02b32fa6f11f0
quat1024 <quat1024@users.noreply.github.com>
2022-12-08 21:47:45 -0500

    08 b (annoying)

--- advent2022/src/day08.rs
+++ advent2022/src/day08.rs
@@ -1,3 +1,7 @@
+use std::ops::Deref;
+use std::ops::Index;
+use std::ops::IndexMut;
+
 use crate::*;
 
 ///consider these as column vectors; the grid
@@ -8,6 +12,22 @@ use crate::*;
 #[derive(Clone, Debug)]
 struct Grid<T>(Vec<Vec<T>>);
 
+//boilerplate to access the grid with grid[x][y] instead of grid.0[x][y]
+impl<T> Index<usize> for Grid<T> {
+	type Output = Vec<T>;
+
+	fn index(&self, index: usize) -> &Self::Output {
+		&self.0[index]
+	}
+}
+
+impl<T> IndexMut<usize> for Grid<T> {
+	fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+		&mut self.0[index]
+	}
+}
+
+//ok
 impl<T> Grid<T> {
 	fn width(&self) -> usize {
 		self.0.len()
@@ -29,15 +49,33 @@ impl<T: Clone> Grid<T> {
 	}
 }
 
-// impl<T: Default> Grid<T> {
-// 	fn clear(&mut self) {
-// 		for column in self.0.iter_mut() {
-// 			for item in column {
-// 				*item = T::default()
-// 			}
-// 		}
-// 	}
-// }
+impl<T: Ord + Copy> Grid<T> {
+	fn max(&self) -> T {
+		//TODO: why .copied() is needed to compile?
+		//ALso yeah it should return an option im just lazy. TODO fix that too
+		self.0.iter().map(|column| column.iter().copied().max().unwrap()).max().unwrap()
+	}
+}
+
+impl<T: ToString> Grid<T> {
+	fn print2(&self) {
+		//this process rotates it 90 degrees the other way i think, lol
+		for column in &self.0 {
+			for item in column {
+				let x = item.to_string();
+				print!(
+					"{}",
+					match x.len() {
+						0 => " ",
+						1 => &x,
+						_ => ">",
+					}
+				);
+			}
+			println!()
+		}
+	}
+}
 
 impl Grid<u8> {
 	fn parsey_parse(input: &str) -> Self {
@@ -65,7 +103,7 @@ impl Grid<bool> {
 		//rotates it 90 degrees
 		for column in &self.0 {
 			for item in column {
-				print!("{}", if *item { '#' } else { ' ' })
+				print!("{}", if *item { '#' } else { '.' })
 			}
 			println!()
 		}
@@ -78,7 +116,7 @@ fn compute_visibility_map(forest: &Grid<u8>) -> Grid<bool> {
 	for x in 0..forest.width() {
 		let mut tallest_tree_from_north = -1i16;
 		for y in 0..forest.height() {
-			let tree_here = forest.0[x][y] as i16;
+			let tree_here = forest[x][y] as i16;
 			if tree_here > tallest_tree_from_north {
 				result.0[x][y] = true;
 				tallest_tree_from_north = tree_here;
@@ -87,7 +125,7 @@ fn compute_visibility_map(forest: &Grid<u8>) -> Grid<bool> {
 
 		let mut tallest_tree_from_south = -1i16;
 		for y in (0..forest.height()).rev() {
-			let tree_here = forest.0[x][y] as i16;
+			let tree_here = forest[x][y] as i16;
 			if tree_here > tallest_tree_from_south {
 				result.0[x][y] = true;
 				tallest_tree_from_south = tree_here;
@@ -98,7 +136,7 @@ fn compute_visibility_map(forest: &Grid<u8>) -> Grid<bool> {
 	for y in 0..forest.height() {
 		let mut tallest_tree_from_west = -1i16;
 		for x in 0..forest.width() {
-			let tree_here = forest.0[x][y] as i16;
+			let tree_here = forest[x][y] as i16;
 			if tree_here > tallest_tree_from_west {
 				result.0[x][y] = true;
 				tallest_tree_from_west = tree_here;
@@ -107,7 +145,7 @@ fn compute_visibility_map(forest: &Grid<u8>) -> Grid<bool> {
 
 		let mut tallest_tree_from_east = -1i16;
 		for x in (0..forest.width()).rev() {
-			let tree_here = forest.0[x][y] as i16;
+			let tree_here = forest[x][y] as i16;
 			if tree_here > tallest_tree_from_east {
 				result.0[x][y] = true;
 				tallest_tree_from_east = tree_here;
@@ -118,16 +156,62 @@ fn compute_visibility_map(forest: &Grid<u8>) -> Grid<bool> {
 	result
 }
 
+fn compute_scenic_scores_map(forest: &Grid<u8>) -> Grid<usize> {
+	let mut scenic_scores = Grid::new_same_size(forest, 0);
+
+	fn count(forest: &Grid<u8>, house: u8, x: usize, y: usize, dx: isize, dy: isize) -> usize {
+		let mut count = 0;
+		for i in 1..isize::MAX {
+			//painstakingly bounds check (extension trait might help here)
+			if let Some(sample_x) = x.checked_add_signed(dx * i).filter(|hm| (0..forest.width()).contains(hm)) {
+				if let Some(sample_y) = y.checked_add_signed(dy * i).filter(|hm| (0..forest.width()).contains(hm)) {
+					//always count the tree, even if it terminates the iteration
+					count += 1;
+					
+					if forest[sample_x][sample_y] >= house {
+						break;
+					}
+					
+				} else {
+					break; //y
+				}
+			} else {
+				break; //x
+			}
+		}
+
+		count
+	}
+
+	for x in 0..forest.width() {
+		for y in 0..forest.height() {
+			//"To measure the viewing distance from a given tree, look up, down, left, and right from that tree;
+			//stop if you reach an edge or at the first tree that is the same height or taller than the tree under consideration."
+			let house = forest[x][y];
+
+			//somewhat pretty, but doesn't work
+			// let up_score    = (1..).take_while(|i| y.checked_sub(*i).is_some()).take_while(|step| forest[x][y - step] <= house).count();
+			// let left_score  = (1..).take_while(|i| x.checked_sub(*i).is_some()).take_while(|step| forest[x - step][y] <= house).count();
+			// let right_score = (1..).take_while(|i| x + i < forest.width()) .take_while(|step| forest[x + step][y] <= house).count();
+			// let down_score  = (1..).take_while(|i| y + i < forest.height()).take_while(|step| forest[x][y + step] <= house).count();
+			let up_score = count(forest, house, x, y, 0, -1);
+			let right_score = count(forest, house, x, y, 1, 0);
+			let down_score = count(forest, house, x, y, 0, 1);
+			let left_score = count(forest, house, x, y, -1, 0);
+
+			scenic_scores[x][y] = up_score * right_score * down_score * left_score
+		}
+	}
+
+	scenic_scores
+}
+
 fn run_a_on(input: String) -> impl Display {
-	let forest = Grid::parsey_parse(&input);
-	forest.print();
-	let visibility_map = compute_visibility_map(&forest);
-	visibility_map.print();
-	visibility_map.population().to_string()
+	compute_visibility_map(&Grid::parsey_parse(&input)).population().to_string()
 }
 
 fn run_b_on(input: String) -> impl Display {
-	"x"
+	compute_scenic_scores_map(&Grid::parsey_parse(&input)).max().to_string()
 }
 
 pub fn run_a() -> impl Display {
@@ -145,12 +229,13 @@ mod test {
 	#[test]
 	fn test() {
 		assert_eq!(run_a_on(test_input_as_string(8)).to_string(), "21");
-		//assert_eq!(run_b_on(test_input_as_string(8)).to_string(), "x");
+		assert_eq!(run_b_on(test_input_as_string(8)).to_string(), "8");
 	}
 
 	#[test]
 	fn real() {
 		assert_eq!(run_a().to_string(), "1688");
-		//assert_eq!(run_b().to_string(), "x");
+		assert_ne!(run_b().to_string(), "5752800"); //too high
+		assert_eq!(run_b().to_string(), "410400");
 	}
 }
