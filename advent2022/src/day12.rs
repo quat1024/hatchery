6e3a27aa1762c9761929975800a0bb8e770494bc
quat1024 <quat1024@users.noreply.github.com>
2022-12-12 02:26:53 -0500

    even more golfin. looks nice now

--- advent2022/src/day12.rs
+++ advent2022/src/day12.rs
@@ -1,6 +1,5 @@
 #![allow(clippy::cast_sign_loss)]
 
-use std::collections::HashMap;
 use std::collections::HashSet;
 
 use crate::*;
@@ -22,14 +21,11 @@ impl Map {
 		}
 	}
 
-	fn pathfinding_data(&self) -> HashMap<Coord, usize> {
-		let mut explored_area = HashMap::new();
-		explored_area.insert(self.end, 0);
-
-		let mut to_explore: HashSet<Coord> = HashSet::new();
+	fn path(&self, mut stopping_condition: impl FnMut(&Coord) -> bool) -> Option<usize> {
+		let mut explored_area = HashSet::new();
+		let mut to_explore = HashSet::new();
 		to_explore.insert(self.end);
-
-		let mut frontier_of_exploration: HashSet<Coord> = HashSet::new();
+		let mut frontier_of_exploration = HashSet::new();
 
 		let mut steps = 0;
 		while !&to_explore.is_empty() {
@@ -37,31 +33,26 @@ impl Map {
 
 			for &coord in &to_explore {
 				let dst = self.get(coord).unwrap();
-
-				for checkpos in [(coord.0 - 1, coord.1), (coord.0 + 1, coord.1), (coord.0, coord.1 - 1), (coord.0, coord.1 + 1)] {
-					if !explored_area.contains_key(&checkpos) && self.get(checkpos).filter(|src| (*src as u8 + 1) >= (dst as u8)).is_some() {
-						match explored_area.get(&checkpos) {
-							Some(&prev) if steps < prev => {
-								frontier_of_exploration.insert(checkpos);
-							},
-							None => {
-								frontier_of_exploration.insert(checkpos);
-							},
-							_ => (),
-						};
+				for src_coord in [(coord.0 - 1, coord.1), (coord.0 + 1, coord.1), (coord.0, coord.1 - 1), (coord.0, coord.1 + 1)]
+					.iter()
+					.filter(|&c| !explored_area.contains(c) && self.get(*c).filter(|src| (*src as u8 + 1) >= (dst as u8)).is_some())
+				{
+					if stopping_condition(src_coord) {
+						return Some(steps);
 					}
+					frontier_of_exploration.insert(*src_coord);
 				}
 			}
 
 			for coord in &frontier_of_exploration {
-				explored_area.insert(*coord, steps);
+				explored_area.insert(*coord);
 			}
 
 			std::mem::swap(&mut to_explore, &mut frontier_of_exploration);
 			frontier_of_exploration.clear();
 		}
 
-		explored_area
+		None
 	}
 }
 
@@ -82,28 +73,24 @@ fn parse(input: &str) -> Map {
 		start: {
 			let (row_id, row) = input.lines().enumerate().find(|(_, row)| row.contains('S')).expect("start exists");
 			let (col_id, _) = row.chars().enumerate().find(|(_, c)| *c == 'S').unwrap();
-			(row_id.try_into().expect("start fits in isize"), col_id.try_into().expect("start fits in isize"))
+			(row_id.try_into().expect("start row fits in isize"), col_id.try_into().expect("start col fits in isize"))
 		},
 		end: {
 			let (row_id, row) = input.lines().enumerate().find(|(_, row)| row.contains('E')).expect("end exists");
 			let (col_id, _) = row.chars().enumerate().find(|(_, c)| *c == 'E').unwrap();
-			(row_id.try_into().expect("end fits in isize"), col_id.try_into().expect("end fits in isize"))
+			(row_id.try_into().expect("end row fits in isize"), col_id.try_into().expect("end col fits in isize"))
 		},
 	}
 }
 
 pub fn a(input: &str) -> impl Display {
 	let map = parse(input);
-	*map.pathfinding_data().get(&map.start).expect("start reachable from end")
+	map.path(|coord| &map.start == coord).expect("start reachable from end")
 }
 
 pub fn b(input: &str) -> impl Display {
 	let map = parse(input);
-	map.pathfinding_data()
-		.into_iter()
-		.filter_map(|(coord, distance)| if matches!(map.get(coord), Some(height) if height == 'a') { Some(distance) } else { None })
-		.min()
-		.expect("at least one start reachable from end")
+	map.path(|coord| matches!(map.get(*coord), Some(height) if height == 'a')).expect("at least one start reachable from end")
 }
 
 #[cfg(test)]
