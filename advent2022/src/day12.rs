a53f2417a258782d5ad489d335154975248d36ac
quat1024 <quat1024@users.noreply.github.com>
2022-12-12 00:59:46 -0500

    12 b. kinda grody

--- advent2022/src/day12.rs
+++ advent2022/src/day12.rs
@@ -25,6 +25,77 @@ impl Map {
 			None
 		}
 	}
+	
+	fn populate_path_lengths_to_end(&self) -> HashMap<Coord, usize> {
+		let mut navigation = HashMap::new();
+		navigation.insert(self.end, 0);
+		
+		for steps in 1.. {
+			let mut found_anything_new = false;
+			for (coord, steps_to_reach) in navigation.clone() {
+				if steps_to_reach == steps - 1 {
+					let dst = self.get(coord).unwrap();
+					
+					//check up one row
+					let up = (coord.0 - 1, coord.1);
+					if self.get(up).filter(|src| can_traverse(*src, dst)).is_some() {
+						let previous_steps_to_reach = *navigation.get(&up).unwrap_or(&1000);
+						if steps < previous_steps_to_reach {
+							found_anything_new |= navigation.insert(up, steps).is_none();
+						}
+					}
+					
+					//check down one row
+					let down = (coord.0 + 1, coord.1);
+					if self.get(down).filter(|src| can_traverse(*src, dst)).is_some() {
+						let previous_steps_to_reach = *navigation.get(&down).unwrap_or(&1000);
+						if steps < previous_steps_to_reach {
+							found_anything_new |= navigation.insert(down, steps).is_none();
+						}
+					}
+					
+					//left one column
+					let left = (coord.0, coord.1 - 1);
+					if self.get(left).filter(|src| can_traverse(*src, dst)).is_some() {
+						let previous_steps_to_reach = *navigation.get(&left).unwrap_or(&1000);
+						if steps < previous_steps_to_reach {
+							found_anything_new |= navigation.insert(left, steps).is_none();
+						}
+					}
+					
+					//right one column
+					let right = (coord.0, coord.1 + 1);
+					if self.get(right).filter(|src| can_traverse(*src, dst)).is_some() {
+						let previous_steps_to_reach = *navigation.get(&right).unwrap_or(&1000);
+						if steps < previous_steps_to_reach {
+							found_anything_new |= navigation.insert(right, steps).is_none();
+						}
+					}
+				}
+			}
+			
+			if !found_anything_new {
+				break
+			}
+		}
+		
+		navigation
+	}
+	
+	fn find_all(&self, what: char) -> Vec<Coord> {
+		let mut result = Vec::new();
+		for row in 0..self.rows() {
+			for col in 0..self.cols() {
+				if let Some(here) = self.get((row as _, col as _)) {
+					if here == what {
+						result.push((row as _, col as _))
+					}
+					
+				}
+			}
+		}
+		result
+	}
 }
 
 fn parse(input: &str) -> Map {
@@ -56,62 +127,24 @@ fn can_traverse(src: char, dst: char) -> bool {
 
 pub fn a(input: &str) -> impl Display {
 	let map = parse(input);
-	let mut navigation: HashMap<Coord, usize> = HashMap::new();
-	navigation.insert(map.end, 0);
-
-	for steps in 1.. {
-		for (coord, steps_to_reach) in navigation.clone() {
-			if steps_to_reach == steps - 1 {
-				let dst = map.get(coord).unwrap();
-				
-				//check up one row
-				let up = (coord.0 - 1, coord.1);
-				if map.get(up).filter(|src| can_traverse(*src, dst)).is_some() {
-					let previous_steps_to_reach = *navigation.get(&up).unwrap_or(&1000);
-					if steps < previous_steps_to_reach {
-						navigation.insert(up, steps);
-					}
-				}
-				
-				//check down one row
-				let down = (coord.0 + 1, coord.1);
-				if map.get(down).filter(|src| can_traverse(*src, dst)).is_some() {
-					let previous_steps_to_reach = *navigation.get(&down).unwrap_or(&1000);
-					if steps < previous_steps_to_reach {
-						navigation.insert(down, steps);
-					}
-				}
-				
-				//left one column
-				let left = (coord.0, coord.1 - 1);
-				if map.get(left).filter(|src| can_traverse(*src, dst)).is_some() {
-					let previous_steps_to_reach = *navigation.get(&left).unwrap_or(&1000);
-					if steps < previous_steps_to_reach {
-						navigation.insert(left, steps);
-					}
-				}
-				
-				//right one column
-				let left = (coord.0, coord.1 + 1);
-				if map.get(left).filter(|src| can_traverse(*src, dst)).is_some() {
-					let previous_steps_to_reach = *navigation.get(&left).unwrap_or(&1000);
-					if steps < previous_steps_to_reach {
-						navigation.insert(left, steps);
-					}
-				}
-			}
-		}
-
-		if navigation.contains_key(&map.start) {
-			return navigation.get(&map.start).unwrap().to_string();
-		}
-	}
-
-	unreachable!()
+	let n = map.populate_path_lengths_to_end();
+	*n.get(&map.start).unwrap()
 }
 
 pub fn b(input: &str) -> impl Display {
-	"x"
+	let map = parse(input);
+	let starts = map.find_all('a');
+	
+	let mut shortest_path = 10000usize;
+	let navigation = map.populate_path_lengths_to_end();
+	
+	for start in starts {
+		if let Some(path) = navigation.get(&start) {
+			shortest_path = shortest_path.min(*path);
+		}
+	}
+	
+	shortest_path
 }
 
 #[cfg(test)]
@@ -121,12 +154,12 @@ mod test {
 	#[test]
 	fn test() {
 		assert_eq!(a(&test_input_as_string(12)).to_string(), "31");
-		assert_eq!(b(&test_input_as_string(12)).to_string(), "x");
+		assert_eq!(b(&test_input_as_string(12)).to_string(), "29");
 	}
 
 	#[test]
 	fn real() {
 		assert_eq!(a(&input_as_string(12)).to_string(), "504");
-		assert_eq!(b(&input_as_string(12)).to_string(), "x");
+		assert_eq!(b(&input_as_string(12)).to_string(), "500");
 	}
 }
