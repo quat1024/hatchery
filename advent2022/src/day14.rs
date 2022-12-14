5ed475f5711faf892ea539d00e8518c9feae0a89
quat1024 <quat1024@users.noreply.github.com>
2022-12-14 01:37:53 -0500

    14 b i cannot believe it actually works LOL

--- advent2022/src/day14.rs
+++ advent2022/src/day14.rs
@@ -47,28 +47,36 @@ struct Map {
 
 impl Map {
 	#[allow(clippy::cast_sign_loss)]
-	fn get(&self, index: Coord) -> Option<Tile> {
-		if let Some(row) = self.map.get(index.0 as usize) {
-			row.get(index.1 as usize).copied()
+	#[allow(clippy::manual_map)] //Im gonna change it
+	fn get(&self, coord: Coord) -> Option<Tile> {
+		if let Some(row) = self.map.get(coord.0 as usize) {
+			//TODO not shit
+			Some(row.get(coord.1 as usize).copied().unwrap_or(Tile::Air))
 		} else {
 			None
 		}
 	}
 
 	#[allow(clippy::cast_sign_loss)]
-	fn set(&mut self, index: Coord, tile: Tile) {
-		self.map[index.0 as usize][index.1 as usize] = tile;
+	#[allow(clippy::cast_possible_wrap)]
+	fn set(&mut self, coord: Coord, tile: Tile) {
+		//TODO not shit
+		if (self.map[coord.0 as usize].len() as isize) <= coord.1 {
+			self.map[coord.0 as usize].extend(std::iter::repeat(Tile::Air).take(40)); //and we hope it's good enough
+		}
+
+		self.map[coord.0 as usize][coord.1 as usize] = tile;
 	}
 
 	#[allow(clippy::cast_sign_loss)]
-	fn parse(input: &str) -> Map {
+	fn parse(input: &str, part2funny: bool) -> Map {
 		let tubes: Vec<Vec<Coord>> = input.lines().map(|tube| tube.split(" -> ").filter_map(parse_coord).collect()).collect();
 
 		let max_row = tubes.iter().flatten().map(|c| c.0).max().unwrap() as usize;
 		let max_col = tubes.iter().flatten().map(|c| c.1).max().unwrap() as usize;
 
 		let blank_row = vec![Tile::Air; max_col + 1];
-		let mut map = Map { map: std::iter::repeat(blank_row).take(max_row + 1).collect() };
+		let mut map = Map { map: std::iter::repeat(blank_row).take(max_row + 10).collect() }; //10 for part2funny buffer
 
 		for tube in tubes {
 			for &[start, end] in tube.array_windows() {
@@ -84,11 +92,24 @@ impl Map {
 			}
 		}
 
+		if part2funny {
+			for col in 0..max_row + 1000 {
+				map.set(((max_row + 2).try_into().unwrap(), col.try_into().unwrap()), Tile::Wall);
+			}
+		}
+
 		map
 	}
 
-	fn drop_sand(&mut self) -> bool {
+	fn drop_sand(&mut self) -> SandDropResult {
 		let mut cursor = (0, 500);
+
+		if let Some(t) = self.get(cursor) {
+			if !t.passable() {
+				return SandDropResult::Blocked;
+			}
+		}
+
 		loop {
 			let below = (cursor.0 + 1, cursor.1);
 
@@ -114,12 +135,11 @@ impl Map {
 				}
 
 				self.set(cursor, Tile::Sand);
-				return true;
+				return SandDropResult::Landed;
 			}
 
 			println!("fall out at {cursor:?}");
-			self.set((cursor.0 - 1, cursor.1), Tile::Sand);
-			return false;
+			return SandDropResult::FellOut;
 		}
 	}
 }
@@ -138,22 +158,29 @@ impl Display for Map {
 	}
 }
 
+#[derive(Copy, Clone, PartialEq, Eq)]
+enum SandDropResult {
+	Landed,
+	FellOut,
+	Blocked,
+}
+
 pub fn a(input: &str) -> impl Display {
-	let mut map = Map::parse(input);
+	let mut map = Map::parse(input, false);
 	let mut grain_count = 0;
-	while map.drop_sand() {
+	while map.drop_sand() == SandDropResult::Landed {
 		grain_count += 1;
 	}
-	
-	//let shit: std::path::PathBuf = [std::env::current_dir().unwrap(), "slkdjakldjlkadjklsad".into()].iter().collect();
-	//std::fs::write(shit, map.to_string()).unwrap();
-	
-	//println!("{map}");
 	grain_count
 }
 
 pub fn b(input: &str) -> impl Display {
-	""
+	let mut map = Map::parse(input, true);
+	let mut grain_count = 0;
+	while map.drop_sand() != SandDropResult::Blocked {
+		grain_count += 1;
+	}
+	grain_count
 }
 
 #[cfg(test)]
@@ -163,12 +190,12 @@ mod test {
 	#[test]
 	fn test() {
 		assert_eq!(a(&test_input_as_string(14)).to_string(), "24");
-		//assert_eq!(b(&test_input_as_string(14)).to_string(), "");
+		assert_eq!(b(&test_input_as_string(14)).to_string(), "93");
 	}
 
 	#[test]
 	fn real() {
-		assert_eq!(a(&input_as_string(14)).to_string(), "592"); //no
-		//assert_eq!(b(&input_as_string(14)).to_string(), "");
+		assert_eq!(a(&input_as_string(14)).to_string(), "592");
+		assert_eq!(b(&input_as_string(14)).to_string(), "30367");
 	}
 }
